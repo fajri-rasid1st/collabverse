974f34e74a470d42d1e7662dfc0c18e85e3db88b
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2025-09-01 14:26:02 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/collabverse/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.collabverse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:collabverse/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CollabVerseApp());
+}
