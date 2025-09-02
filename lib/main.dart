4388f5086e9f0b5737b7033296a57c23da8caa04
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2025-09-02 14:56:06 +0700

    add launcher icon and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
-import 'package:collabverse/app.dart';
-import 'package:collabverse/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:collabverse/app.dart';
+import 'package:collabverse/firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  // Prevent landscape orientation
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FlutterNativeSplash.remove();
 
   runApp(const CollabVerseApp());
 }
