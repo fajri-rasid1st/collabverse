b2754893e63ed1fcc4974f93613d6ca245843d56
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2025-09-11 10:16:13 +0700

    update splash screen

--- lib/app.dart
+++ lib/app.dart
@@ -7,6 +7,7 @@ class CollabVerseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'CollabVerse',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
